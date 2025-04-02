10ae7071cfeee32a202aa75db03739b1bf47b755
lagisreal89 <lagisrealyt@gmail.com>
2025-04-01 20:43:54 -0400

    Changed the bounding of the map

--- wya-niners/src/components/MapComponent.jsx
+++ wya-niners/src/components/MapComponent.jsx
@@ -1,9 +1,10 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import L from "leaflet";
+import { useEffect, useState } from 'react';
 const MapComponent = () => {
   return (
-    <MapContainer center={[35.308767382976846, -80.7337804153427]} zoom={17} style={{ height: '100%', width: '100%' }}>
+    <MapContainer center={[35.308767382976846, -80.7337804153427]} zoom={17} style={{ height: '100%', width: '100%' }} maxBounds={[[-90, -180],[90, 180],]} maxBoundsViscosity={1.0} maxZoom={18} minZoom={2}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
